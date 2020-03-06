240642eca3b4b22ae019ace6b8c72ee29a7d35e3
Mark Butcher <markjamesbutcher@gmail.com>
2020-03-06 15:59:01 +1100

    homepage css

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,18 +2,34 @@ import React from "react";
 import "./homepage.style.scss";
 import { Link } from "react-router-dom";
 
-const HomePage = ({ currentUser }) => (
-  <>
-    <div className="homepage">
-      {/* <img className="homeImage" src={Diamond} /> */}
-      <Link className="btn-enter" to="/menu" style={{ textDecoration: "none" }}>
-        enter
-      </Link>
-      {currentUser ? (
-        <span className="user-name">Hello => {currentUser.displayName}</span>
-      ) : null}
-    </div>
-  </>
-);
+const HomePage = ({ currentUser }) => {
+  const firstName = () => currentUser.displayName.split(" ")[0];
+
+  return (
+    <>
+      <div className="homepage">
+        {/* <img className="homeImage" src={Diamond} /> */}
+
+        {currentUser ? (
+          <Link
+            className="btn-enter"
+            to="/menu"
+            style={{ textDecoration: "none" }}
+          >
+            Welcome back {firstName(firstName)}
+          </Link>
+        ) : (
+          <Link
+            className="btn-enter"
+            to="/menu"
+            style={{ textDecoration: "none" }}
+          >
+            enter
+          </Link>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
